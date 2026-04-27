dade9dfe1de0bd0806bf2f3343722c95429fb039
Jens Pfleghaar <jpflegha@student.42heilbronn.de>
2026-04-27 17:36:06 +0200

    meaningfule command

--- src/requirements/mariadb/tools/init.sh
+++ src/requirements/mariadb/tools/init.sh
@@ -9,7 +9,7 @@ if [ ! -d "/var/lib/mysql/mysql" ]; then
 mysql_install_db --user=mysql --datadir=/var/lib/mysql > /dev/null
 DB_PASSWORD=$(cat /run/secrets/db_password)
 DB_ROOT_PASSWORD=$(cat /run/secrets/db_root_password)
-mysqld --user=mysql --bootstrap << EOF
+mysqld --user=mysql --bootstrap <<EOF
 FLUSH PRIVILEGES;
 CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
 CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';
