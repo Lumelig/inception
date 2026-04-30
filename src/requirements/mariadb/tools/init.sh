bb5bff27d2684a702351a050451f4436ca7188fb
Jens Pfleghaar <jpflegha@student.42heilbronn.de>
2026-04-30 15:43:43 +0200

    still try to run mariadb

--- src/requirements/mariadb/tools/init.sh
+++ src/requirements/mariadb/tools/init.sh
@@ -6,10 +6,17 @@ chown -R mysql:mysql /run/mysqld
 
 #This check prevents re-initializing every time
 if [ ! -d "/var/lib/mysql/mysql" ]; then
-mysql_install_db --user=mysql --datadir=/var/lib/mysql > /dev/null
-DB_PASSWORD=$(cat /run/secrets/db_password)
-DB_ROOT_PASSWORD=$(cat /run/secrets/db_root_password)
-mysqld --user=mysql --bootstrap <<EOF
+    echo ">>> First time init — creating database and user..."
+    mysql_install_db --user=mysql --datadir=/var/lib/mysql > /dev/null
+    
+    DB_PASSWORD=$(cat /run/secrets/db_password)
+    DB_ROOT_PASSWORD=$(cat /run/secrets/db_root_password)
+    
+    echo ">>> DB_PASSWORD is: $DB_PASSWORD"
+    echo ">>> MYSQL_USER is: $MYSQL_USER"
+    echo ">>> MYSQL_DATABASE is: $MYSQL_DATABASE"
+    
+    mysqld --user=mysql --bootstrap <<EOF
 FLUSH PRIVILEGES;
 CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
 CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';
@@ -17,6 +24,9 @@ GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
 ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';
 FLUSH PRIVILEGES;
 EOF
-fi 
+    echo ">>> Init done!"
+else
+    echo ">>> Database already exists, skipping init"
+fi
 
 exec mysqld --user=mysql
